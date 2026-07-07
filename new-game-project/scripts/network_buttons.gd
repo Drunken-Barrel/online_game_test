5eb545c33ff4bbd9b3124b5dea637edfb8c202d8
drunken_barrel <joedell0@outlook.com>
2026-07-07 16:39:44 +1000

    Buttons now hide properly and player is heavy

--- new-game-project/scripts/network_buttons.gd
+++ new-game-project/scripts/network_buttons.gd
@@ -1,5 +1,9 @@
 extends Control
 
+func _ready() -> void:
+	if NetworkHandler.IP_ADDRESS != "LocalHost":
+		visible = false
+
 func _on_server_button_pressed() -> void:
 	NetworkHandler.start_server()
 	visible = false
