c2c964eecd1ee45941659dfe254e9dfb36f5063b
drunken_barrel <joedell0@outlook.com>
2026-07-08 11:55:57 +1000

    added semi functional bars
    still need to check a few things and figure out how to not require ArmourBar to stop it from breaking

--- new-game-project/scripts/player.gd
+++ new-game-project/scripts/player.gd
@@ -1,10 +1,56 @@
 extends CharacterBody2D
-class_name Player
+
+# game stat variables
 @export var speed: int = 300
+# defence/armour stats
+@export var max_defence = 500:
+	set(value):
+		max_defence = value
+		DefenceBar.max_value = max_defence
+var defence: int = 0:
+	set(value):
+		if value > max_defence:
+			value = max_defence
+		defence = value
+		if defence <= 0:
+			armour_break()
+		print("defence updated")
+		DefenceBar.value = defence
+# health stats
+@export var max_health = 500:
+	set(value):
+		max_health = value
+		HealthBar.max_value = max_health
+var health: int = 0:
+	set(value):
+		if value > max_health:
+			value = max_health
+		health = value
+		if health <= 0:
+			die()
+		print("health updated")
+		HealthBar.value = health
+# stamina stats
+@export var max_stamina = 500:
+	set(value):
+		max_stamina = value
+		StaminaBar.max_value = max_stamina
+var stamina: int = 0:
+	set(value):
+		if value > max_stamina:
+			value = max_stamina
+		stamina = value
+		print("stamina updated")
+		StaminaBar.value = stamina
+
+# Logic variables
 @export var CustomMultiplayerSpawner: PackedScene
 @onready var screen_size: Vector2 = get_viewport_rect().size
 @onready var PlayerSprite: Sprite2D = $PlayerSprite
 @onready var PlayerCamera: Camera2D = $PlayerCamera
+@onready var DefenceBar: ProgressBar = $StatBars/DefenceBar
+@onready var HealthBar: ProgressBar = $StatBars/HealthBar
+@onready var StaminaBar: ProgressBar = $StatBars/StaminaBar
 
 func _ready() -> void:
 	# connect the disconnect signal to the despawn func
@@ -12,12 +58,16 @@ func _ready() -> void:
 	# make the player camera the current camera for the client controlling them
 	if is_multiplayer_authority():
 		PlayerCamera.make_current()
+	# move the player to the middle (will be changed to spawn points when menus are added)
+	position = Vector2(1000,200)
+	# set all the stats to the starting numbers
+	defence = max_defence
+	health = max_health
+	stamina = max_stamina
 
 func _enter_tree() -> void:
 	# give the client authority over their player
 	set_multiplayer_authority(name.to_int())
-	# move the player to the middle (will be changed to spawn points when menus are added)
-	position = Vector2(1000,200)
 
 func _process(_delta: float) -> void:
 	# kick out anyone who isn't the player in control
@@ -45,6 +95,15 @@ func _process(_delta: float) -> void:
 	# make the player sprite point towards their cusor (doesn't affect hitbox)
 	PlayerSprite.rotation = get_angle_to(get_global_mouse_position())
 
-func despawn_player(id):
+# despawns the player when the disconnect
+func despawn_player(id) -> void:
 	if id == int(name):
 		queue_free()
+
+# manages what happens when the player's armour breaks
+func armour_break() -> void:
+	pass
+
+# manages what happens when the player dies
+func die() -> void:
+	pass
